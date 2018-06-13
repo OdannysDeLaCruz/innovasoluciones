89d129d0e14c8ed4811e7b5ea424728ba10bf08e
Odannys De La Cruz <el_odanis321@hotmail.com>
2018-06-13 11:01:26 -0500

    Creacion del archivo header.php, inclución de este archivo en la secciones requeridas de los otros archivos .php

--- detalles.php
+++ detalles.php
@@ -11,29 +11,8 @@
 	<title> Detalles del producto | Innova Soluciones </title>
 </head>
 <body>
-	<header class="header_principal">
-		<a class="header_principal_img" href="/">
-			<img src="img/logos/LogoInnova.svg" alt="Logotipo de Innova Soluciones">
-		</a>
-		<div class="contenedor_formulario">
-			<form class="formulario_buscar_producto" action="/search" method="post">
-				<input class="input_buscador_producto" type="text" name="buscar" placeholder="Busca por categorias" required>
-				<button class="btn_buscar" type="submit"><span class="fa fa-search"></span></button>
-			</form>
-		</div>
-		<nav class="nav_principal">
-			<span class="fa fa-times cerrar_menu" id="cerrar_menu"></span>
-			<ul>
-				<li><a href="/">Home</a></li>
-				<li><a href="/productos.php">Productos</a></li>
-				<li><a href="/login.php">Entrar</a></li>
-				<li><a href="/register.php">Registrarse</a></li>
-			</ul>
-		</nav>
-		<span id="carrito_icono" class="fa fa-cart-plus carrito_icono"></span>
-		<span class="fa fa-bars abrir_menu"></span>
-	</header>
 	
+	<?php include_once('header.php'); ?>	
 	<?php include_once('carrito.php'); ?>
 
 	<nav class="nav_categorias">
