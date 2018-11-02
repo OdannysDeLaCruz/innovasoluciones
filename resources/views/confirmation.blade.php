f237e11361b8dbb9fe658f2fe34c2e2a239336c0
Odannys De La Cruz <el_odanis321@hotmail.com>
2018-11-02 11:42:37 -0500

    Prueba de borrado de carrito 3

--- resources/views/confirmation.blade.php
+++ resources/views/confirmation.blade.php
@@ -3,12 +3,12 @@ session_start();
 unset($_SESSION['cart']); 
 session_destroy();
 
-if(strlen($datos)>0){
-	$filename = "pruebas.txt";
-	$fp = fopen($filename, "a");
-	fwrite($fp, 'Datos enviados');
-	fwrite($fp, $datos, strlen($datos));
-	fclose($fp);
-}
+// if(strlen($datos)>0){
+// 	$filename = "pruebas.txt";
+// 	$fp = fopen($filename, "a");
+// 	fwrite($fp, 'Datos enviados');
+// 	fwrite($fp, $datos, strlen($datos));
+// 	fclose($fp);
+// }
 
 
