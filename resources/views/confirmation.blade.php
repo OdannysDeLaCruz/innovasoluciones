a3af1e22783e824a82d17a1e6bb1b46d975d5428
Odannys De La Cruz <el_odanis321@hotmail.com>
2018-11-02 11:05:05 -0500

    Prueba de borrado de carrito en heroku dependiendo de los datos que vienen de payu

--- resources/views/confirmation.blade.php
+++ resources/views/confirmation.blade.php
@@ -5,14 +5,14 @@ if(strlen($datos)>0){
 	$fp = fopen($filename, "a");
 	if($fp) {
 		fwrite($fp, $datos, strlen($datos));
-	fclose($fp);
+		fclose($fp);
 
 	}
+	// Elimino todos los datos de session con respecto al carrito de compra
+	session()->forget('cart');
+	session()->forget('codigos_usados');				
+	session()->forget('descuento_peso');				
+	session()->forget('notificacion_codigo');
 }
 
-// Elimino todos los datos de session con respecto al carrito de compra
-session()->forget('cart');
-session()->forget('codigos_usados');				
-session()->forget('descuento_peso');				
-session()->forget('notificacion_codigo');
 
