58b7d7147127dc9e2bebb575360ce9ddc09dea87
Odannys De La Cruz <el_odanis321@hotmail.com>
2018-11-08 12:19:08 -0500

    subida a heroku 3

--- app/Http/Controllers/ConfirmationController.php
+++ app/Http/Controllers/ConfirmationController.php
@@ -125,66 +125,66 @@ class ConfirmationController extends Controller
 
 
 
-		$state_pol            = $_POST['state_pol'];
-		$response_message_pol = $_POST['response_message_pol'];
-		$response_code_pol    = $_POST['response_code_pol'];
+		// $state_pol            = $_POST['state_pol'];
+		// $response_message_pol = $_POST['response_message_pol'];
+		// $response_code_pol    = $_POST['response_code_pol'];
 
-		$id_user   = Auth::user()->id;
-		$comprador = Auth::user()->nombre . " " . Auth::user()->apellido;
-		$ref_venta = $_POST['reference_sale'];
-		$direccion_envio = $_POST['shipping_address'];
+		// $id_user   = Auth::user()->id;
+		// $comprador = Auth::user()->nombre . " " . Auth::user()->apellido;
+		// $ref_venta = $_POST['reference_sale'];
+		// $direccion_envio = $_POST['shipping_address'];
 
-		$payment_method_id = $_POST['payment_method_id'];
-		switch ($payment_method_id) {
-			case 2:$medio_pago = 'CREDIT_CARD'; break;
-			case 4: $medio_pago = '	PSE'; break;
-			case 5: $medio_pago = 'ACH'; break;
-			case 6: $medio_pago = 'DEBIT_CARD'; break;
-			case 7: $medio_pago = 'CASH'; break;
-			case 8: $medio_pago = 'REFERENCED'; break;
-			case 10: $medio_pago = 'BANK_REFERENCED'; break;
-			case 14: $medio_pago = 'SPEI'; break;
-		}
-		$payment_method_name = $_POST['payment_method_name'];
-		$date = $_POST['date'];
+		// $payment_method_id = $_POST['payment_method_id'];
+		// switch ($payment_method_id) {
+		// 	case 2:$medio_pago = 'CREDIT_CARD'; break;
+		// 	case 4: $medio_pago = '	PSE'; break;
+		// 	case 5: $medio_pago = 'ACH'; break;
+		// 	case 6: $medio_pago = 'DEBIT_CARD'; break;
+		// 	case 7: $medio_pago = 'CASH'; break;
+		// 	case 8: $medio_pago = 'REFERENCED'; break;
+		// 	case 10: $medio_pago = 'BANK_REFERENCED'; break;
+		// 	case 14: $medio_pago = 'SPEI'; break;
+		// }
+		// $payment_method_name = $_POST['payment_method_name'];
+		// $date = $_POST['date'];
 
-    	if($state_pol == 4 && $response_message_pol == 'APPROVED' && $response_code_pol == 1) {
-    		try {
-				App\Pedido::create([ 
-				    'id_user'         => $id_user,
-			        'comprador'       => $comprador,
-			        'ref_venta'       => 'prueba',
-			        'direccion_envio' => 'prueba',
-			        'modo_pago'       => 'prueba',
-			        'codigo_descuento'=> 'prueba',
-			        'modo_envio'      => 'prueba',
-			        'estado_pedido'   => 'prueba',
-			        'fecha_pedido'    => $date,
-			    ]);
+  //   	if($state_pol == 4 && $response_message_pol == 'APPROVED' && $response_code_pol == 1) {
+  //   		try {
+		// 		App\Pedido::create([ 
+		// 		    'id_user'         => $id_user,
+		// 	        'comprador'       => $comprador,
+		// 	        'ref_venta'       => 'prueba',
+		// 	        'direccion_envio' => 'prueba',
+		// 	        'modo_pago'       => 'prueba',
+		// 	        'codigo_descuento'=> 'prueba',
+		// 	        'modo_envio'      => 'prueba',
+		// 	        'estado_pedido'   => 'prueba',
+		// 	        'fecha_pedido'    => $date,
+		// 	    ]);
 
-				// Si se ha creado el pedido correctamente, enviar un correo de confirmacion al usuario
-			    $fp = fopen('pruebas.txt', "a");
-				if($fp) {
-					fwrite($fp, 'Pedido creado');
-					fclose($fp);
-				}
-			}
-			catch (Exception $e) {
-				// Si hay algun error al intentar crear el pedido, enviar un correo a soporte tecnico de Innova.
-				$msm $e->getMessage();
-				$fp = fopen('pruebas.txt', "a");
-				if($fp) {
-					fwrite($fp, 'Error al intentar crear pedido : ' . $msm);
-					fclose($fp);
-				}
-			}
-    	}
-        else {
-        	$fp = fopen('pruebas.txt', "a");
-			if($fp) {
-				fwrite($fp, 'No se ha podido crear el pedido, estado de pedido rechazado');
-				fclose($fp);
-			}
-		}
+		// 		// Si se ha creado el pedido correctamente, enviar un correo de confirmacion al usuario
+		// 	    $fp = fopen('pruebas.txt', "a");
+		// 		if($fp) {
+		// 			fwrite($fp, 'Pedido creado');
+		// 			fclose($fp);
+		// 		}
+		// 	}
+		// 	catch (Exception $e) {
+		// 		// Si hay algun error al intentar crear el pedido, enviar un correo a soporte tecnico de Innova.
+		// 		$msm $e->getMessage();
+		// 		$fp = fopen('pruebas.txt', "a");
+		// 		if($fp) {
+		// 			fwrite($fp, 'Error al intentar crear pedido : ' . $msm);
+		// 			fclose($fp);
+		// 		}
+		// 	}
+  //   	}
+  //       else {
+  //       	$fp = fopen('pruebas.txt', "a");
+		// 	if($fp) {
+		// 		fwrite($fp, 'No se ha podido crear el pedido, estado de pedido rechazado');
+		// 		fclose($fp);
+		// 	}
+		// }
     }
 }
